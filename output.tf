af76bc72319d40b89b3a52eb9bacf16ca8f05447
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2019-07-21 16:26:48 +0800

    Removed old logging exports

--- output.tf
+++ output.tf
@@ -1,24 +1,6 @@
 ##export the logging details for EH and SA
 ##export one object with both
-/*
-output "eh_logs_id" {
-  depends_on = [azurerm_eventhub_namespace.log]
 
-  value = azurerm_eventhub_namespace.log.id
-}
-
-output "eh_logs_name" {
-  depends_on = [azurerm_eventhub_namespace.log]
-
-  value = azurerm_eventhub_namespace.log.name
-}
-
-output "sa_logs_id" {
-  depends_on = [azurerm_storage_account.log]
-
-  value = azurerm_storage_account.log.id
-}
-*/
 output "diagnostics_map" {
     depends_on = [
         azurerm_storage_account.log, 
